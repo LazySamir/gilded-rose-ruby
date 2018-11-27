2841ac07b5727862544b09100d72608c742d80e0
Samir <samirsoormally@gmail.com>
2018-11-27 15:10:30 +0000

    tests: added for Sulfuras item

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,12 +4,8 @@ describe GildedRose do
   let(:items) { [
     Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
     Item.new(name="Aged Brie", sell_in=2, quality=0),
-    Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
     Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-    Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
     Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
     # This Conjured item does not work properly yet
     Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
   ] }
@@ -17,6 +13,7 @@ describe GildedRose do
   subject(:gilded_rose) { GildedRose.new(items) }
 
   describe "#update_quality" do
+    
     describe "for normal items" do
       describe "item quality" do
         it "decreases by 1" do
@@ -54,5 +51,20 @@ describe GildedRose do
         end  
       end
     end
+    
+    describe "for Sulfuras" do
+      describe "item quality" do
+        it "does not change" do
+          expect{gilded_rose.update_quality}.to_not change{items[2].quality}
+        end
+      end
+      describe "item sell_in value" do
+        it "does not change" do
+          expect{gilded_rose.update_quality}.to_not change{items[2].sell_in}
+        end  
+      end
+    end
+    
+    
   end
 end
