5c311ef5b4d0fc5da356d269c48ad0c4c7d0f1e3
Samir <samirsoormally@gmail.com>
2018-11-27 14:49:54 +0000

    tests: added for normal items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,34 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require_relative "../gilded_rose"
 
 describe GildedRose do
+  let(:items) { [
+    Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)
+  ] }
+  
+  subject(:gilded_rose) { GildedRose.new(items) }
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+    describe "for normal items" do
+      
+      describe "item quality" do
+        it "decreases by 1" do
+          expect{gilded_rose.update_quality}.to change{items[0].quality}.by(-1)
+        end
+        it "does not decrease below 0" do
+          items[0].quality = 0
+          expect{gilded_rose.update_quality}.to_not change{items[0].quality}
+        end
+        it "decreases x2 past sell_in date" do
+          items[0].sell_in = 0
+          expect{gilded_rose.update_quality}.to change{items[0].quality}.by(-2)
+        end
+      end
+        
+      describe "item sell_in value" do
+        it "decreases by 1" do
+          expect{gilded_rose.update_quality}.to change{items[0].sell_in}.by(-1)
+        end  
+      end
     end
   end
-
 end
